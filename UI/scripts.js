d6e70fdfc9ae187c78bd9f9ced03e10ef0906287
faksam <fakunlesamuel@gmail.com>
2018-04-19 18:56:03 +0100

    design customer page

--- UI/scripts.js
+++ UI/scripts.js
@@ -23,48 +23,59 @@ $(document).ready(function(){
     });
 
     // Get the signupModal
-    var signUpModal = document.getElementById('signupModal');
+    
     // Get the button that opens the modal
-    var signUpBtn = document.getElementById("signupBtn");
+    //Get signupModal
+    var signUpModal = document.getElementById('signupModal');
+    // Get the signinModal
+    var signInModal = document.getElementById('signinModal');
+
+    //Get Meal Modal
+
+
     // Get the <span> element that closes the modal
+
+    // Get the button that opens the modal
+    var signInBtn = document.getElementById("signinBtn");
+    var signUpBtn = document.getElementById("signupBtn");
+
+  if (signUpBtn != null || signInBtn != null) {
     var signUpSpan = document.getElementById("signupSpan");
     var signInSpan = document.getElementById("signinSpan");
 
-    // Get the signinModal
-    var signInModal = document.getElementById('signinModal');
-    // Get the button that opens the modal
-    var signInBtn = document.getElementById("signinBtn");
     // Get the <span> element that closes the modal
 
     var span = document.getElementsByClassName("close")[0];
 
     // When the user clicks the button, open the modal 
-    signUpBtn.onclick = function() {
+    signUpBtn.onclick = function () {
       signUpModal.style.display = "block";
-  }
+    }
 
     // When the user clicks the button, open the modal 
-    signInBtn.onclick = function() {
+    signInBtn.onclick = function () {
       signInModal.style.display = "block";
-  }
-
-    // When the user clicks on <span> (x), close the modal
-    signUpSpan.onclick = function() {
-      signUpModal.style.display = "none";
     }
-    signInSpan.onclick = function() {
-      signInModal.style.display = "none";
+    if(signUpSpan != null || signInSpan != null)
+    {
+      // When the user clicks on <span> (x), close the modal
+      signUpSpan.onclick = function () {
+        signUpModal.style.display = "none";
+      }
+      signInSpan.onclick = function () {
+        signInModal.style.display = "none";
+      }  
     }
-
 
     // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function(event) {
-        if (event.target == signUpModal || event.target == signInModal) {
-          signUpModal.style.display = "none";
-          signInModal.style.display = "none";
-        }
+    window.onclick = function (event) {
+      if (event.target == signUpModal || event.target == signInModal) {
+        signUpModal.style.display = "none";
+        signInModal.style.display = "none";
+      }
     }
-
+  }
+    
   }); // end DOM ready
 
 
