ba5d16a25887d8c31d0768a8aea4c70671a6aab9
faksam <fakunlesamuel@gmail.com>
2018-04-19 02:51:38 +0100

    design index page

--- UI/scripts.js
+++ UI/scripts.js
@@ -1,4 +1,7 @@
 $(document).ready(function(){   
+   
+   
+   //Navigation
    // If a link has a dropdown, add sub menu toggle.
     $('nav ul li a:not(:only-child)').click(function(e) {
       $(this).siblings('.nav-dropdown').toggle();
@@ -18,6 +21,50 @@ $(document).ready(function(){
     $('#nav-toggle').on('click', function() {
       this.classList.toggle('active');
     });
+
+    // Get the signupModal
+    var signUpModal = document.getElementById('signupModal');
+    // Get the button that opens the modal
+    var signUpBtn = document.getElementById("signupBtn");
+    // Get the <span> element that closes the modal
+    var signUpSpan = document.getElementById("signupSpan");
+    var signInSpan = document.getElementById("signinSpan");
+
+    // Get the signinModal
+    var signInModal = document.getElementById('signinModal');
+    // Get the button that opens the modal
+    var signInBtn = document.getElementById("signinBtn");
+    // Get the <span> element that closes the modal
+
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks the button, open the modal 
+    signUpBtn.onclick = function() {
+      signUpModal.style.display = "block";
+  }
+
+    // When the user clicks the button, open the modal 
+    signInBtn.onclick = function() {
+      signInModal.style.display = "block";
+  }
+
+    // When the user clicks on <span> (x), close the modal
+    signUpSpan.onclick = function() {
+      signUpModal.style.display = "none";
+    }
+    signInSpan.onclick = function() {
+      signInModal.style.display = "none";
+    }
+
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == signUpModal || event.target == signInModal) {
+          signUpModal.style.display = "none";
+          signInModal.style.display = "none";
+        }
+    }
+
   }); // end DOM ready
 
 
