35cf51a45104ef4e96d913a137ca3e0c235a697d
faksam <fakunlesamuel@gmail.com>
2018-04-19 23:00:20 +0100

    create caterers page

--- UI/scripts.js
+++ UI/scripts.js
@@ -29,6 +29,8 @@ $(document).ready(function(){
     var signUpModal = document.getElementById('signupModal');
     // Get the signinModal
     var signInModal = document.getElementById('signinModal');
+    // Get the catererSigninModal
+    var catererSigninModal = document.getElementById('catererSigninModal');
 
     //Get Meal Modal
 
@@ -38,10 +40,12 @@ $(document).ready(function(){
     // Get the button that opens the modal
     var signInBtn = document.getElementById("signinBtn");
     var signUpBtn = document.getElementById("signupBtn");
+    var catererSigninBtn = document.getElementById("catererSigninBtn");
 
-  if (signUpBtn != null || signInBtn != null) {
+  if (signUpBtn != null || signInBtn != null || catererSigninBtn) {
     var signUpSpan = document.getElementById("signupSpan");
     var signInSpan = document.getElementById("signinSpan");
+    var catererSignInSpan = document.getElementById("catererSigninSpan");
 
     // Get the <span> element that closes the modal
 
@@ -56,7 +60,12 @@ $(document).ready(function(){
     signInBtn.onclick = function () {
       signInModal.style.display = "block";
     }
-    if(signUpSpan != null || signInSpan != null)
+
+    // When the user clicks the button, open the modal 
+    catererSigninBtn.onclick = function () {
+      catererSigninModal.style.display = "block";
+    }
+    if(signUpSpan != null || signInSpan != null || catererSignInSpan != null)
     {
       // When the user clicks on <span> (x), close the modal
       signUpSpan.onclick = function () {
@@ -65,13 +74,17 @@ $(document).ready(function(){
       signInSpan.onclick = function () {
         signInModal.style.display = "none";
       }  
+      catererSigninSpan.onclick = function () {
+        catererSigninModal.style.display = "none";
+      }  
     }
 
     // When the user clicks anywhere outside of the modal, close it
     window.onclick = function (event) {
-      if (event.target == signUpModal || event.target == signInModal) {
+      if (event.target == signUpModal || event.target == signInModal || event.target == catererSigninModal) {
         signUpModal.style.display = "none";
         signInModal.style.display = "none";
+        catererSigninModal.style.display = "none";
       }
     }
   }
