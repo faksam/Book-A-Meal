ad5ebfd1fdefb754e5c7279ae3145be2fb92ea3d
faksam <fakunlesamuel@gmail.com>
2018-04-26 19:01:02 +0100

    update controller

--- server/controllers/menu.js
+++ server/controllers/menu.js
@@ -1,8 +1,16 @@
 // import json file
 import menu from '../book-meal/menu.json';
 
+
 /*
- * POST /menu to save a new menu.
+ * GET /meal route to retrieve all the menu.
+ */
+function getMenu(req, res) {
+  res.send(menu);
+}
+
+/*
+ * POST /meal to save a new meal.
  */
 function postMenu(req, res) {
   menu.menu.push(req.body);
@@ -10,9 +18,33 @@ function postMenu(req, res) {
   res.status(201).send(menu);
 }
 
+/*
+ * GET /menu/:id route to retrieve a menu given its id.
+ */
+function getMenuItem(req, res) {
+  function findMenu(menuItem) {
+    return menuItem.id === req.params.id;
+  }
+  res.send(menu.menu.find(findMenu));
+}
+
 
 /*
- * PUT /menu/:id to updatea a menu given its id
+ * DELETE /meal/:id to delete a meal given its id.
+ */
+function deleteMenu(req, res) {
+  let count = 0;
+  menu.menu.forEach((element) => {
+    if (element.id === req.params.id) {
+      menu.menu.splice(count, 1);
+    }
+    count += 1;
+  });
+  res.send(menu);
+}
+
+/*
+ * PUT /meal/:id to updatea a meal given its id
  */
 function updateMenu(req, res) {
   menu.menu.forEach((element, index) => {
--- server/routes/v1/menu.js
+++ server/routes/v1/menu.js
@@ -1,6 +1,9 @@
 import menuCtrl from '../../controllers/menu';
 
 module.exports = (app) => {
+  app.route('/menu/').get(menuCtrl.getMenu);
   app.route('/menu').post(menuCtrl.postMenu);
+  app.route('/menu/:id').get(menuCtrl.getMenuItem);
+  app.route('/menu/:id').delete(menuCtrl.deleteMenu);
   app.route('/menu/:id').put(menuCtrl.updateMenu);
 };
