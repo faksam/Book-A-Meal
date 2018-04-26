afbfc3f71e6e23e51c46af73dadde11117ccc734
faksam <fakunlesamuel@gmail.com>
2018-04-26 14:49:35 +0100

    update models index file

--- server/models/index.js
+++ server/models/index.js
@@ -1,55 +1,11 @@
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
-const basename = path.basename(module.filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(`${__dirname}/../config/config.json`)[env];
-const db = {};
+// const fs = require('fs');
+// const path = require('path');
+// const Sequelize = require('sequelize');
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable]);
-} else {
-  sequelize = new Sequelize(
-    config.database, config.username, config.password, config
-  );
-}
-
-fs
-  .readdirSync(__dirname)
-  .filter(file =>
-    (file.indexOf('.') !== 0) &&
-    (file !== basename) &&
-    (file.slice(-3) === '.js'))
-  .forEach(file => {
-    const model = sequelize.import(path.join(__dirname, file));
-    db[model.name] = model;
-  });
-
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
-});
-
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
-
-module.exports = db;
-
-// import fs from 'fs';
-// import path from 'path';
-// import Sequelize from 'sequelize';
-// import appConfig from '../config/config.json';
-
-// // const basename = path.basename(module.filename);
-// // const env = process.env.NODE_ENV || 'development';
-// // const config = require(`${__dirname}/../config/config.js`)[env];
-
-// const basename = path.basename(__filename);
+// const basename = path.basename(module.filename);
 // const env = process.env.NODE_ENV || 'development';
-// // const config = con[env];
-// const config = appConfig[env];
+// // const config = require(`${__dirname}/../config/config.json`)[env];
+// const config = require(`${__dirname}/../config/config.js`)[env];
 // const db = {};
 
 // let sequelize;
@@ -80,6 +36,50 @@ module.exports = db;
 // db.Sequelize = Sequelize;
 
 // module.exports = db;
+
+import fs from 'fs';
+import path from 'path';
+import Sequelize from 'sequelize';
+import appConfig from '../config/config.json';
+
+// const basename = path.basename(module.filename);
+// const env = process.env.NODE_ENV || 'development';
+// const config = require(`${__dirname}/../config/config.js`)[env];
+
+const basename = path.basename(__filename);
+const env = process.env.NODE_ENV || 'development';
+// const config = con[env];
+const config = appConfig[env];
+const db = {};
+
+let sequelize;
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable]);
+} else {
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
+
+fs
+  .readdirSync(__dirname)
+  .filter(file =>
+    (file.indexOf('.') !== 0) &&
+    (file !== basename) &&
+    (file.slice(-3) === '.js'))
+  .forEach((file) => {
+    const model = sequelize.import(path.join(__dirname, file));
+    db[model.name] = model;
+  });
+
+Object.keys(db).forEach((modelName) => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
 
 // const fs = require('fs');
 // const path = require('path');
