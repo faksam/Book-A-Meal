0fb90b177376f3e115a6600503a0212977690b5d
faksam <fakunlesamuel@gmail.com>
2018-04-26 20:23:34 +0100

    update test file

--- server/controllers/orders.js
+++ server/controllers/orders.js
@@ -1,11 +1,55 @@
 // import json file
 import orders from '../book-meal/orders.json';
 
-
 /*
- * GET /orders route to retrieve all the orders.
+ * GET /meal route to retrieve all the orders.
  */
 function getOrders(req, res) {
+  res.send(orders);
+}
+
+/*
+ * POST /orders to save a new order.
+ */
+function postOrder(req, res) {
+  orders.orders.push(req.body);
+
+  res.status(201).send(orders);
+}
+
+/*
+ * GET /meal/:id route to retrieve a meal given its id.
+ */
+function getOrder(req, res) {
+  function findOrder(meal) {
+    return meal.id === req.params.id;
+  }
+  res.send(orders.orders.find(findOrder));
+}
+
+/*
+ * DELETE /meal/:id to delete a meal given its id.
+ */
+function deleteOrder(req, res) {
+  let count = 0;
+  orders.orders.forEach((element) => {
+    if (element.id === req.params.id) {
+      orders.orders.splice(count, 1);
+    }
+    count += 1;
+  });
+  res.send(orders);
+}
+
+/*
+ * PUT /meal/:id to updatea a meal given its id
+ */
+function updateOrder(req, res) {
+  orders.orders.forEach((element, index) => {
+    if (element.id === req.params.id) {
+      orders.orders[index] = req.body;
+    }
+  });
   res.send(orders);
 }
 
