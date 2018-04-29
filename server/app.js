b78df5dacd0aaeaf95980337ff890258c12df55e
faksam <fakunlesamuel@gmail.com>
2018-04-29 07:49:25 +0100

    update app.js

--- server/app.js
+++ server/app.js
@@ -54,27 +54,27 @@ app.use((req, res, next) => {
 });
 
 // error handler
-// app.use((err, req, res, next) => {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use((err, req, res, next) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 // production error handler
 // no stacktraces leaked to user
-app.use((err, req, res, next) => {
-  if (err.status !== 500) {
-    res.status(err.status);
-  } else { res.status(500); }
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
+// app.use((err, req, res, next) => {
+//   if (err.status !== 500) {
+//     res.status(err.status);
+//   } else { res.status(500); }
+//   res.render('error', {
+//     message: err.message,
+//     error: {}
+//   });
+// });
 
 
 module.exports = app;
