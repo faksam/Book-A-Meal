c110c4f34ac246914730892a5874c4bb86ed9c55
Fakunle Mayowa Samuel <fakunlesamuel@gmail.com>
2018-04-29 22:54:31 +0100

    Revert "Ft create meal 156806998"

--- server/app.js
+++ server/app.js
@@ -46,7 +46,6 @@ app.use('../UI', express.static(path.join(`${__dirname}../UI`)));
 // app.route('/meals/:id').delete(meals.deleteMeal);
 // app.route('/meals/:id').put(meals.updateMeal);
 
-
 app.use('/*', indexRouter);
 
 // catch 404 and forward to error handler
